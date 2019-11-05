c4a79c285e8dcf981279c87f7a4eecf5fcb87a61
sodeh <sodeh.tarahomi@yahoo.com>
2019-11-05 17:29:15 -0500

    test commit

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+                Console.Write("please your number:");
+                int num1 = int.Parse(Console.ReadLine());
+                int sum = 0;
+                while (num1 != 0)
+                {
+                    int num2 = num1 % 10;
+                    sum = sum + num2;
+                    num1 = num1 / 10;
+                }
+
+                Console.WriteLine("the sum of digit this number is: {0}", sum);
+
+                Console.ReadKey();
+
+            }
+        }
+    }
+
---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter your num:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            while (num != 0)
+            {
+                for (int j = num; j > 0; j--)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+                num--;
+
+            }
+               
+
+            Console.ReadKey();
+        }
+    }
+}
